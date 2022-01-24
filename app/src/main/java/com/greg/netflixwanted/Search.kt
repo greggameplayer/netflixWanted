e332933a0ccf1a1f0cf002fafe0c5877c1327b20
Noémie LECHERF <noemie.lecherf@epsi.fr>
2022-01-24 18:22:31 +0100

    debut vue inscription mais bug avec image

---
+++ app/src/main/java/com/greg/netflixwanted/Search.kt
@@ -0,0 +1,12 @@
+package com.greg.netflixwanted
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Search : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search)
+    }
+}
