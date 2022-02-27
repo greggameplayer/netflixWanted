2c22ffb3addcb3dedcc3a56797dbe31efb6693e4
Unknown <gregoire.hage@gmail.com>
2022-02-27 15:07:18 +0100

    limit api calls

--- app/src/main/java/com/greg/netflixwanted/SearchActivity.kt
+++ app/src/main/java/com/greg/netflixwanted/SearchActivity.kt
@@ -1,10 +1,53 @@
 package com.greg.netflixwanted
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-class SearchActivity  : AppCompatActivity() {
+import com.greg.netflixwanted.controllers.RetrofitController
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+class SearchActivity  : AppCompatActivity(), CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    private lateinit var job: Job
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        job = Job()
         setContentView(R.layout.activity_search)
+
+        val compositeDisposable = CompositeDisposable()
+
+        compositeDisposable.add(
+            RetrofitController.getRetrofitServiceMongo().getApiCalls(body = RetrofitController.getRetrofitFindOneParam())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({response -> onResponse(response)}, {t -> onFailure(t) }))
+
+        compositeDisposable.add(
+            RetrofitController.getRetrofitServiceMongo().updateApiCalls(body = RetrofitController.getRetrofitUpdateOneParam())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({response -> onResponse(response)}, {t -> onFailure(t) }))
+    }
+
+    override fun onDestroy() {
+        job.cancel() // cancel the Job
+        super.onDestroy()
+    }
+
+    private fun onFailure(t: Throwable) {
+        Toast.makeText(this,t.message, Toast.LENGTH_SHORT).show()
+        println(t.message)
+    }
+
+    private fun onResponse(response: Any) {
+        Toast.makeText(this,response.toString(), Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/greg/netflixwanted/controllers/RetrofitController.kt
+++ app/src/main/java/com/greg/netflixwanted/controllers/RetrofitController.kt
@@ -1,18 +1,90 @@
 package com.greg.netflixwanted.controllers
 
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import com.greg.netflixwanted.beans.RetrofitConfig.Companion.BASE_URL
+import com.greg.netflixwanted.beans.RetrofitConfig.Companion.BASE_URL_MONGO
 import com.greg.netflixwanted.interfaces.RetrofitService
+import com.greg.netflixwanted.interfaces.RetrofitServiceMongo
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import org.json.JSONException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 
 class RetrofitController {
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    companion object {
+        private val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-    fun getRetrofitService() = retrofit.create(RetrofitService::class.java)
+        private val retrofitDataMongo = Retrofit.Builder()
+            .baseUrl(BASE_URL_MONGO)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        fun getRetrofitService() = retrofit.create(RetrofitService::class.java)
+
+        fun getRetrofitServiceMongo() = retrofitDataMongo.create(RetrofitServiceMongo::class.java)
+
+        fun getRetrofitFindOneParam(): JsonObject {
+            var gsonObject = JsonObject()
+
+            try {
+                val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                val jsonObject = JsonObject()
+                val nestedObject = JsonObject()
+                nestedObject.addProperty("date", dateFormat.format(LocalDate.now()))
+                jsonObject.addProperty("dataSource", "Cluster0")
+                jsonObject.addProperty("database", "netflixWanted")
+                jsonObject.addProperty("collection", "apiCalls")
+                jsonObject.add("filter", nestedObject)
+
+                val jsonParser = JsonParser()
+                gsonObject = jsonParser.parse(jsonObject.toString()) as JsonObject
+
+                println("MY gson.JSON:  AS PARAMETER  $gsonObject")
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+
+            return gsonObject
+
+        }
+
+        fun getRetrofitUpdateOneParam(): JsonObject {
+            var gsonObject = JsonObject()
+
+            try {
+                val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                val jsonObject = JsonObject()
+                val nestedObject = JsonObject()
+                val setNestedObject = JsonObject()
+                val incNestedObject = JsonObject()
+                incNestedObject.addProperty("calls", -1)
+                setNestedObject.add("\$inc", incNestedObject)
+                nestedObject.addProperty("date", dateFormat.format(LocalDate.now()))
+                jsonObject.addProperty("dataSource", "Cluster0")
+                jsonObject.addProperty("database", "netflixWanted")
+                jsonObject.addProperty("collection", "apiCalls")
+                jsonObject.add("filter", nestedObject)
+                jsonObject.add("update", setNestedObject)
+
+                val jsonParser = JsonParser()
+                gsonObject = jsonParser.parse(jsonObject.toString()) as JsonObject
+
+                println("MY gson.JSON:  AS PARAMETER  $gsonObject")
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+
+            return gsonObject
+        }
+    }
 }
