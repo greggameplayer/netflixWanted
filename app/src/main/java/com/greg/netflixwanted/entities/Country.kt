7daa8f6e299bea4e0aef1e8251dd80a4c64ecf02
victor.marit <victormarit.95@gmail.com>
2022-02-28 20:31:55 +0100

    Modification de l'entité pour prendre en compte les drapeaux

--- app/src/main/java/com/greg/netflixwanted/entities/Country.kt
+++ app/src/main/java/com/greg/netflixwanted/entities/Country.kt
@@ -5,4 +5,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "country")
-class Country (@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int, @ColumnInfo(name = "name") val name: String, @ColumnInfo(name = "countrycode") val countrycode: String)
+class Country (@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int, @ColumnInfo(name = "name") val name: String, @ColumnInfo(name = "countrycode") val countrycode: String, @ColumnInfo(name = "img") val img: String)
